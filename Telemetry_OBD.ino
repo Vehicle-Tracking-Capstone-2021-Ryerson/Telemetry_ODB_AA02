c7c1eefca079de8e210b583507e200b3123940d5
Dan Stingaciu <dsstingaciu@gmail.com>
2022-03-26 20:26:17 -0400

    Created new buffers for incoming data, updated throttle position PID

--- Telemetry_OBD.ino
+++ Telemetry_OBD.ino
@@ -18,7 +18,7 @@ mcp2515_can CAN(SPI_CS_PIN);
 // Define PIDs we want to gather
 #define PID_ENGINE_RPM 0x0C
 #define PID_CAR_SPEED 0x0D
-#define PID_THROTTLE_POS 0x11
+#define PID_THROTTLE_POS 0x45
 #define PID_AMBIENT_TEMP 0x46
 #define PID_FUEL_LEVEL 0x2F
 
@@ -54,8 +54,8 @@ void set_mask_filt()
 void sendPid(unsigned char __pid)
 {
   unsigned char tmp[8] = {0x02, 0x01, __pid, 0, 0, 0, 0, 0};
-  SERIAL_PORT_MONITOR.print("SEND PID: 0x");
-  SERIAL_PORT_MONITOR.println(__pid, HEX);
+  //SERIAL_PORT_MONITOR.print("SEND PID: 0x");
+  //SERIAL_PORT_MONITOR.println(__pid, HEX);
   CAN.sendMsgBuf(CAN_ID_PID, 0, 8, tmp);
 }
 
@@ -80,115 +80,153 @@ void setup()
 
 void loop()
 {
-  unsigned char buf[8];
-  int __rpm = 0, __speed = 0, __throttle = 0, __airTemp = 0, __fuel_level = 0;
-
+  unsigned char buf1[8], buf2[8], buf3[8], buf4[8], buf5[8];
+  char rpmStr[4], speedStr[4], throttleStr[4], airTempStr[4], fuelStr[4], printBuf[32];
+  float __rpm = 0.0, __speed = 0.0, __throttle = 0.0, __airTemp = 0.0;
+  float __fuel_level = 0.0;
   // Send RPM Request
   sendPid(PID_ENGINE_RPM);
-  delay(500);      // Wait a bit
-  getDataBuf(buf); // Get data
+  delay(100);        // Wait a bit
+  getDataBuf(buf1); // Get data
 
-  if (buf[0] != 0x0)
+  if (buf1[0] != 0x0)
   {
-    extractRPM(buf, &__rpm);
+    __rpm = extractRPM(buf1);
   }
+  
+  dtostrf(__rpm, 4, 2, rpmStr);
+  strcpy(printBuf, rpmStr);
+  strcat(printBuf, ",");
 
-  SERIAL_PORT_MONITOR.print("ENGINE RPM: ");
-  SERIAL_PORT_MONITOR.println(__rpm);
+  // SERIAL_PORT_MONITOR.print("ENGINE RPM: ");
+  // SERIAL_PORT_MONITOR.println(__rpm);
 
   // Send Car speed request
   sendPid(PID_CAR_SPEED);
-  delay(500);      // Wait a bit
-  getDataBuf(buf); // Get Data
+  delay(100);        // Wait a bit
+  getDataBuf(buf2); // Get Data
 
-  if (buf[0] != 0x0)
+  if (buf2[0] != 0x0)
   {
-    extractVehicleSpeed(buf, &__speed);
+    __speed = extractVehicleSpeed(buf2);
   }
 
-  SERIAL_PORT_MONITOR.print("VEHICLE SPEED: ");
-  SERIAL_PORT_MONITOR.println(__speed);
+  dtostrf(__speed, 4, 2, speedStr);
+  strcat(printBuf, speedStr);
+  strcat(printBuf, ",");
+
+  // SERIAL_PORT_MONITOR.print("VEHICLE SPEED: ");
+  // SERIAL_PORT_MONITOR.println(__speed);
 
   // Send throttle position request
   sendPid(PID_THROTTLE_POS);
-  delay(500);      // Wait a bit
-  getDataBuf(buf); // Get data
+  delay(100);        // Wait a bit
+  getDataBuf(buf3); // Get data
 
-  if (buf[0] != 0x0)
+  if (buf3[0] != 0x0)
   {
-    extractThrottlePos(buf, &__throttle);
+    __throttle = extractThrottlePos(buf3);
   }
 
-  SERIAL_PORT_MONITOR.print("THROTTLE POS: ");
-  SERIAL_PORT_MONITOR.println(__throttle);
+  dtostrf(__throttle, 4, 2, throttleStr);
+  strcat(printBuf, throttleStr);
+  strcat(printBuf, ",");
+
+
+  // SERIAL_PORT_MONITOR.print("THROTTLE POS: ");
+  // SERIAL_PORT_MONITOR.println(__throttle);
 
   // Send air temperature position request
   sendPid(PID_AMBIENT_TEMP);
-  delay(500);      // Wait a bit
-  getDataBuf(buf); // Get data
+  delay(100);        // Wait a bit
+  getDataBuf(buf4); // Get data
 
-  if (buf[0] != 0x0)
+  if (buf4[0] != 0x0)
   {
-    extractAmbientTemp(buf, &__airTemp);
+    __airTemp = extractAmbientTemp(buf4);
   }
 
-  SERIAL_PORT_MONITOR.print("AIR TEMPERATURE: ");
-  SERIAL_PORT_MONITOR.println(__airTemp);
+  dtostrf(__airTemp, 4, 2, airTempStr);
+  strcat(printBuf, airTempStr);
+  strcat(printBuf, ",");
+
+  // SERIAL_PORT_MONITOR.print("AIR TEMPERATURE: ");
+  // SERIAL_PORT_MONITOR.println(__airTemp);
 
   // Send throttle position request
   sendPid(PID_FUEL_LEVEL);
-  delay(500);      // Wait a bit
-  getDataBuf(buf); // Get data
+  delay(100);      // Wait a bit
+  getDataBuf(buf5); // Get data
 
-  if (buf[0] != 0x0)
+  if (buf5[0] != 0x0)
   {
-    extractThrottlePos(buf, &__fuel_level);
+    __fuel_level = extractFuelLevel(buf5);
   }
 
-  SERIAL_PORT_MONITOR.print("FUEL LEVEL: ");
-  SERIAL_PORT_MONITOR.println(__fuel_level);
+  dtostrf(__fuel_level, 4, 2, fuelStr);
+  strcat(printBuf, fuelStr);
 
-  SERIAL_PORT_MONITOR.println(String(__rpm) + "," + String(__speed) + "," + String(__throttle) + "," + String(__airTemp) + "," + String(__fuel_level));
+  // SERIAL_PORT_MONITOR.print("FUEL LEVEL: ");
+  // SERIAL_PORT_MONITOR.println(__fuel_level);
+  Serial.println(printBuf);
+  delay(1000);
 }
 
-bool extractRPM(unsigned char *buf, int *rpm)
+float extractRPM(unsigned char *buf)
 {
+  float rpm;
   if (buf[1] == 0x41)
   {
-    *rpm = (256 * buf[3] * buf[4]) / 4;
-    return 1;
+    float pre = 256 * buf[3] + buf[4];
+    rpm = pre / 4;
+    return rpm;
   }
-  return 0;
+  return -1;
 }
 
-bool extractVehicleSpeed(unsigned char *buf, int *vehicle_speed)
+float extractVehicleSpeed(unsigned char *buf)
 {
+  float vehicle_speed;
   if (buf[1] == 0x41)
   {
-    *vehicle_speed = buf[3];
-    return 1;
+    vehicle_speed = (int) buf[3];
+    return vehicle_speed;
   }
-  return 0;
+  return -1;
 }
 
-bool extractThrottlePos(unsigned char *buf, int *throttle)
+float extractThrottlePos(unsigned char *buf)
 {
+  float throttle;
   if (buf[1] == 0x41)
   {
-    *throttle = 100 / 255 * buf[3];
-    return 1;
+    throttle = 0.39 * buf[3];
+    return throttle;
   }
-  return 0;
+  return -1;
 }
 
-bool extractAmbientTemp(unsigned char *buf, int *temp)
+float extractAmbientTemp(unsigned char *buf)
 {
+  float temp;
   if (buf[1] == 0x41)
   {
-    *temp = buf[3] - 40;
-    return 1;
+    temp = buf[3] - 40;
+    return temp;
   }
-  return 0;
+  return -1;
+}
+
+float extractFuelLevel(unsigned char *buf)
+{
+  float fuel;
+  if (buf[1] == 0x41)
+  {
+    double mult = 0.39216;
+    fuel = buf[3] * mult;
+    return fuel;
+  }
+  return -1;
 }
 
 // Extract data frame from request
